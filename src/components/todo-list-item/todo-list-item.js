d06986445b0f99470e5a4362fd64d5096ff96283
Alexey <lbonanzza@gmail.com>
2020-01-30 13:49:02 +0300

    Added delete item

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -12,21 +12,25 @@ export default class TodoListItem extends Component {
     };
 
     this.onMarkImportant = () => {
-      this.setState({
-        important: true
+      this.setState((state) => {
+        return {
+          important: !state.important
+        }
       })
     };
 
     this.onLabelClick = () => {
-      this.setState({
-        done: true
+      this.setState((state) => {
+        return {
+          done: !state.done
+        }
       })
     };
   }
 
   render() {
 
-    const { label } = this.props;
+    const { label, onDeleted } = this.props;
     const { done, important } = this.state;
 
     let classNames = 'todo-list-item';
@@ -46,14 +50,15 @@ export default class TodoListItem extends Component {
         {label}
       </span>
 
-      <button type="button"
+      <button type="button"     // Important button
               className="btn btn-outline-success btn-sm float-right"
               onClick={ this.onMarkImportant } >
         <i className="fa fa-exclamation" />
       </button>
 
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
+      <button type="button"     // Deleted button
+              className="btn btn-outline-danger btn-sm float-right"
+              onClick={onDeleted}>
         <i className="fa fa-trash-o" />
       </button>
     </span>
