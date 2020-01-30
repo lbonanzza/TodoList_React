b965fe971392bd2a87839a286c2f5737d2094633
Alexey <lbonanzza@gmail.com>
2020-01-30 13:03:42 +0300

    Added event handlers for important and completed events

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,26 +3,52 @@ import './todo-list-item.css'
 
 export default class TodoListItem extends Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      done: false,
+      important: false,
+    };
+
+    this.onMarkImportant = () => {
+      this.setState({
+        important: true
+      })
+    };
+
+    this.onLabelClick = () => {
+      this.setState({
+        done: true
+      })
+    };
+  }
 
   render() {
 
-    const { label, important } = this.props;
+    const { label } = this.props;
+    const { done, important } = this.state;
 
-    const style = {
-      color: important ? 'steelblue' : 'black',
-      fontWeight: important ? 'bold' : 'normal'
-    };
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
+
+    if (important) {
+      classNames += ' important';
+    }
 
     return (
-      <span className="todo-list-item">
+      <span className={ classNames }>
       <span
         className="todo-list-item-label"
-        style={style}>
+        onClick={ this.onLabelClick }>
         {label}
       </span>
 
       <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
+              className="btn btn-outline-success btn-sm float-right"
+              onClick={ this.onMarkImportant } >
         <i className="fa fa-exclamation" />
       </button>
 
